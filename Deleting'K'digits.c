55c06b851c525fb4ead3d0b7c8a7ed7856c1078d
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2019-07-24 19:07:36 +0530

    Create Deleting'K'digits.c

---
+++ Deleting'K'digits.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+ int data;
+ struct data* next;
+}*top=NULL,*temp=NULL;
+
+
+void push(int value)
+{
+  // struct node* temp;
+   temp=(struct node*)malloc(sizeof(struct node));
+   temp->data=value;
+   temp->next=NULL;
+
+    if(top==NULL)
+    {
+        top=temp;
+
+
+    }
+    else
+    {
+          temp->next=top;
+          top=temp;
+          temp=NULL;
+    }
+}
+void pop()
+{
+   temp=top;
+   top=top->next;
+ free(temp);
+}
+
+
+
+
+void peek()
+{
+    printf("%d\n",top->data);
+}
+
+void all()
+{
+    while(top!=NULL)
+    {
+        printf("%d",top->data);
+        top=top->next;
+    }
+
+
+}
+
+int main()
+{
+    int n,a,sum=0,t;
+    scanf("%d",&n);
+
+    while(n>0)
+    {
+        a=n%10;
+        n=n/10;
+
+        push(a);
+
+    }
+    scanf("%d",&t);
+    pop(t);
+    all();
+}
+
+
+
+
+
+
+
+
